46c8a297ed3a486ec1f2476d42ce84f5f8d57e5f
Ojas <ojas.modak23@gmail.com>
2023-09-02 23:44:58 +0530

    removed unnecessary

--- mysite/static/ftp/profile/assets/js/view_profile.js
+++
@@ -1,6 +0,0 @@
-// Your main starts here
-about = document.getElementById("one");
-acad = document.getElementById("two");
-contact = document.getElementById("three");
-
-about.click();
--- mysite/static/ftp/project_details/js/js_QgdZ7h4eCpAKLmNOvvnzyYfhnxH5vJdGBL7pD9hKg2E.js
+++
@@ -1,2 +0,0 @@
-(function(Drupal){'use strict';Drupal.behaviors.addToAny={attach:function(context,settings){if(context!==document&&window.a2a){a2a.init_all();}}};})(Drupal);;(function(drupalSettings){'use strict';window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;if(!drupalSettings.ga){return;}
-for(var i=0;i<drupalSettings.ga.commands.length;i++){ga.apply(this,drupalSettings.ga.commands[i]);}})(drupalSettings);;
